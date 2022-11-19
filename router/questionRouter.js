f5fef64540540316ab5bcaaabdd02aaa5efae81a
Rachappa <97609401+rachappa-web-tech@users.noreply.github.com>
2022-11-19 22:12:28 +0530

    ak

--- router/questionRouter.js
+++ router/questionRouter.js
@@ -2,9 +2,7 @@ const router = require('express').Router()
 const questioncontrol  = require('../controler/questionControl')
 const auth =require('../middleware/auth')
 
-
-
 router.post('/add',questioncontrol.add)
 router.get('/data',auth,questioncontrol.data)
-router.get('/question_anser',questioncontrol.question_anser)
+//router.get('/question_anser',questioncontrol.question_anser)
 module.exports = router
